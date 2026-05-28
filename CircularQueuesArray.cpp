13ec442266c0844e61c533398302083191b01fbb
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-05-28 11:18:57 +0700

    menambahkan validasi pilihan menu dan exception handling

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -161,6 +161,20 @@ int main()
                 return 0;
             }
 
+           default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
+        }
+
+        catch (exception &e)
+        {
+            cout << "check for the values entered!" << endl;
         }
     }
+
+    return 0;
+    
 }
