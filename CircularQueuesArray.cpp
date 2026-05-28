0715993d814bad0175896c53840587ea0671c78a
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-05-28 10:47:12 +0700

    menambahkan pengecekan apakah antrian penuh

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -23,5 +23,12 @@ public:
         cin >> num;
         cout << endl;
         
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "Queue overflow";
+            return;
+        }
+
     }
 }
